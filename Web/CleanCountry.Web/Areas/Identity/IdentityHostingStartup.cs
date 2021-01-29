a602c43239b87034f07e48990efcc2cbf97eb330
VencislavKA <57410823+VencislavKA@users.noreply.github.com>
2021-01-29 13:01:05 +0200

    login and register design icons in nav and projects design started

---
+++ Web/CleanCountry.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using CleanCountry.Data;
+using CleanCountry.Data.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(CleanCountry.Web.Areas.Identity.IdentityHostingStartup))]
+namespace CleanCountry.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}

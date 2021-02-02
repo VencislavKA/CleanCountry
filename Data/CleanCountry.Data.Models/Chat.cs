e378fcaf7ca0d3116d091bcb2c45ebdbebdd6b2c
Ibryam <57998847+Ibryam512@users.noreply.github.com>
2021-02-02 17:32:08 +0200

    created Event and Chat models

---
+++ Data/CleanCountry.Data.Models/Chat.cs
@@ -0,0 +1,13 @@
+﻿namespace CleanCountry.Data.Models
+{
+    using CleanCountry.Data.Common.Models;
+
+    public class Chat : BaseDeletableModel<int>
+    {
+        public int UserID { get; set; }
+
+        public string Message { get; set; }
+
+        public int EventID { get; set; }
+    }
+}
---
+++ Data/CleanCountry.Data.Models/Event.cs
@@ -0,0 +1,16 @@
+﻿namespace CleanCountry.Data.Models
+{
+
+    using System.Collections.Generic;
+
+    using CleanCountry.Data.Common.Models;
+
+    public class Event : BaseDeletableModel<int>
+    {
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public List<int> Partisipants { get; set; }
+    }
+}

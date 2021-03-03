02c2821d141a7b0aa276fa4793d59dcd6aae57cc
Ibryam <57998847+Ibryam512@users.noreply.github.com>
2021-03-03 17:14:36 +0200

    work on calendar and home page

--- Web/CleanCountry.Web/Controllers/CelendarController.cs
+++ Web/CleanCountry.Web/Controllers/CelendarController.cs
@@ -5,13 +5,28 @@
     using System.Linq;
     using System.Threading.Tasks;
 
+    using CleanCountry.Data.Models;
+    using CleanCountry.Services.Data;
+    using Microsoft.AspNetCore.Identity;
     using Microsoft.AspNetCore.Mvc;
+
 
     public class CelendarController : BaseController
     {
+        public IProjectsService Service { get; }
+
+        public UserManager<ApplicationUser> UserManager { get; }
+
+        public CelendarController(IProjectsService service, UserManager<ApplicationUser> userManager)
+        {
+            this.Service = service;
+            this.UserManager = userManager;
+        }
+
         public IActionResult Index()
         {
-            return this.View();
+            var projects = this.Service.GetAllProjects().Where(x => x.Partisipants.Contains(this.UserManager.GetUserAsync(this.User).Result)).ToList();
+            return this.View(projects);
         }
     }
 }
